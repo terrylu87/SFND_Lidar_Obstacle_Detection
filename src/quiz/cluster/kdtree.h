2351b552145560715f6e31ca2122ad39a807f06e
TerryLu <terry.lu@aptiv.com>
2019-09-06 15:21:35 +0800

    TODO::Finish search in kdtree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -29,16 +29,66 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+        Node *n = new Node(point,id);
+        if(root==NULL){
+            root = n;
+        }else{
+            int depth = 0;
+            //bool inserted = false;
+            Node *current_node = root;
+            while(1){
+                int idx=depth%2;
+                if(point[idx] < current_node->point[idx]){
+                    //if(point[idx] < (current_node->point)[idx]){
+                    // go left
+                    if(current_node->left == NULL){
+                        current_node->left = n;
+                        break;
+                    }else{
+                        current_node = current_node->left;
+                        ++depth;
+                        //continue;
+                    }
+                }else{
+                    if(current_node->right == NULL){
+                        current_node->right = n;
+                        break;
+                    }else{
+                        current_node = current_node->right;
+                        ++depth;
+                        //continue;
+                    }
+                }
+            }
+        }
 
-	}
+    }
 
-	// return a list of point ids in the tree that are within distance of target
+    float distance(std::vector<float> point0, std::vector<float> point1)
+    {
+        int dims = point0.size();
+        float dist = 0;
+        int i;
+        for(i=0;i<dims;++i){
+            dist += pow(point0[i]-point1[i],2);
+        }
+        dist = sqrt(dist);
+        return dist;
+    }
+
+    // return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+        Node *current_node = root;
+        while(1){
+            if(distance(current_node->point,target) < distanceTol){
+                ids.push_back(current_node->id);
+                continue;
+            }
+        }
 		return ids;
 	}
-	
 
 };
 
